fe35ff9671f55a589a1fe06e1a07d3d0f865300f
Arrttyy <ironman60@live.com.mx>
2024-12-16 14:53:36 -0600

    Added delete

--- php/delete.php
+++ php/delete.php
@@ -5,6 +5,7 @@ $host = 'localhost';
 $dbName = 'compaccser';
 $username = 'root';
 $password = '';
+$aidi = $_GET['id'];
 
 try {
   $pdo = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
@@ -12,3 +13,16 @@ try {
 } catch (PDOException $e) {
   die("Error de conexión a la base de datos: " . $e->getMessage());
 }
+
+// Obtener el último número de orden de la base de datos y sumarle 1
+try {
+  $pdo->beginTransaction();
+  $stmt = $pdo->query("DELETE from ordenes where no_Orden=$aidi");
+
+
+  $pdo->commit();
+} catch (PDOException $e) {
+  $pdo->rollBack();
+  die("Error al obtener el número de orden: " . $e->getMessage());
+}
+echo "<script>window.close();</script>";
